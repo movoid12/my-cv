fc0a2cbacf9ee3a9f61961136af665bed0accee1
Mouaz Aldakkak <87415713+movoid12@users.noreply.github.com>
2025-02-20 15:01:13 +0000

    fix: change default locale from German to English

--- src/utils/locales.ts
+++ src/utils/locales.ts
@@ -2,4 +2,4 @@ export const SUPPORTED_LOCALES = ['de', 'en'] as const;
 
 export type SupportedLocale = (typeof SUPPORTED_LOCALES)[number];
 
-export const DEFAULT_LOCALE: SupportedLocale = 'de';
+export const DEFAULT_LOCALE: SupportedLocale = 'en';
