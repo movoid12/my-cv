88fc5adaa8329ace764b034762d2c4ab85bcab49
Mouaz Aldakkak <apothekendatabase@gmail.com>
2025-02-21 21:45:09 +0100

    feat: add secondary language conditional fetching

--- src/providers/data-provider.tsx
+++ src/providers/data-provider.tsx
@@ -2,6 +2,8 @@ import { createContext, useContext } from 'react';
 // biome-ignore lint/style/useNamingConvention: <explanation>
 import useSWRImmutable from 'swr/immutable';
 import { configs } from '../utils/configs';
+import { useLocale } from '../hooks/use-locale';
+import { DEFAULT_LOCALE } from '../utils/locales';
 
 type SocialLink = {
   linkedin: string;
@@ -79,11 +81,19 @@ const fetcher = (url: RequestInfo | URL) =>
   fetch(url).then((res) => res.json());
 
 export function DataProvider({ children }: { children: React.ReactNode }) {
+  const { locale: currentLocale } = useLocale();
+
+  const isDefaultLocale = currentLocale === DEFAULT_LOCALE;
+
+  const currentApiUrl = isDefaultLocale
+    ? configs.primaryLanguageApiUrl
+    : configs.secondaryLanguageApiUrl;
+
   const {
     data,
     error,
     isLoading: loading,
-  } = useSWRImmutable(`${configs.apiUrl}`, fetcher, {
+  } = useSWRImmutable(`${currentApiUrl}`, fetcher, {
     suspense: true,
   });
 
--- src/utils/configs.ts
+++ src/utils/configs.ts
@@ -1,9 +1,11 @@
 import { z } from 'zod';
 
 const configSchema = z.object({
-  apiUrl: z.string().optional(),
+  primaryLanguageApiUrl: z.string().optional(),
+  secondaryLanguageApiUrl: z.string().nullish(),
 });
 
 export const configs = configSchema.parse({
-  apiUrl: import.meta.env.VITE_CONFIG_API_URL,
+  primaryLanguageApiUrl: import.meta.env.VITE_CONFIG_API_URL_PRIMARY_LOCALE,
+  secondaryLanguageApiUrl: import.meta.env.VITE_CONFIG_API_URL_SECONDARY_LOCALE,
 });
